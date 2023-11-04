2be7452fd23e5b4d733e075cf67340f3de3939ba
YacineAbdelouhab <118994777+YacineAbdelouhab@users.noreply.github.com>
2023-11-05 00:54:33 +0100

    Update InnerNode.java
    Ici, on avance que vers un seul noeud, j'ai fait comme si le graphe est le graphe de l'histoire

--- projet/src/representation/InnerNode.java
+++ projet/src/representation/InnerNode.java
@@ -1,14 +1,15 @@
 package Representation ;
 public class InnerNode extends Node{
 
-  public InnerNode(String description,Node nextNode1,Node nextNode2,Node nextNode3,Node nextNode4){
-    super(description,nextNode1,nextNode2,nextNode3,nextNode4);
+  public InnerNode(String description,Node nextNode1){
+    super(description,nextNode1);
   }
-  public InnerNode(String description,Node nextNode1,Node nextNode2,Node nextNode3,Node nextNode4,Personnages perso1, Personnages perso2,Personnages perso3,Personnages perso4,Objet objet_possede,Nourriture nourriture_possede){
-    super(description,nextNode1,nextNode2,nextNode3,nextNode4,perso1,perso2,perso3,perso4,objet_possede,nourriture_possede);
+  public InnerNode(String description,Node nextNode1,Personnages perso1, Personnages perso2,Personnages perso3,Personnages perso4,Objet objet_possede,Nourriture nourriture_possede){
+    super(description,nextNode1,perso1,perso2,perso3,perso4,objet_possede,nourriture_possede);
   }
+
   public Node chooseNext(){
-    
+    return super.nextNode1;
   }
 
 }
