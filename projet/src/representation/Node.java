72a318e6a58333a5e88ccda094f1fc7b20a42fd1
YacineAbdelouhab <118994777+YacineAbdelouhab@users.noreply.github.com>
2023-11-08 22:59:47 +0100

    Update Node.java
    Rajout des codes javadocs

--- projet/src/representation/Node.java
+++ projet/src/representation/Node.java
@@ -3,26 +3,56 @@ import Univers.Personnages;
 import Univers.Objet;
 import Univers.Nourriture;
 
+/** Un Node constitue les elements dans la cave :
+    -le personnage 1
+    -le personnage 2
+    -le personnage 3
+    -le personnage 4
+    -les objets en possession
+    -la nourriture en possession
+    -le nombre de jour passe dans la cave
+    (cela constitue en parti les attributs de la classe)
+
+
+    L'etat du Node (donc de la cave), change de jour en jour (par l'utilisation de methode) :
+    -On pourra nourir les personnes
+    -On pourra emmener quelquns en expedition a l'exterieur pour recuperer un ou plusieurs objets
+    -On proposera chaque jour qui passe, un certain scenario. Le choix de scnerio est determine en fonction
+     de l'etat de du Node (donc de la cave).
+
+
+@author ABDELOUHAB Yacine
+@version une_seul_classe
+*/
+
+
 public class Node{
 
-    //Personnages Objet et Nourriture sont des varaibles qui sont passe par reference
+    //Personnages Objet et Nourriture sont des variables qui sont passe par reference
     // On pourra donc les modifie par la suite : c'est leurs reference qui est final
+
     private final Personnages p1;
     private final Personnages p2;
     private final Personnages p3;
     private final Personnages p4;
-    private final  Objet objet_possession;
+    private final Objet objet_possession;
     private final Nourriture nourriture_possession;
-    private static  int nombre_journee = 0;
+
+    /** Nombre de jour passe dans la cave
+    */
+    private static  int nombre_journee = 0; //Determine ensuite un nombre de jour max
 
     private String description_debut_journee = "";
     private String description_expedition = "";
     private String description_scenario  = "";
 
 
-    //Constructeur : pas de constructeur par defaut.
-    //On ne l'appelera que une fois, pour indiquer les personnages choisi,
-    //et les objets/nourritures choisi par le joeur
+
+
+    /**Constructeur, il n'y a pas de constructeur par defaut.
+       On ne l'appelera que une fois, pour indiquer les personnages, les objets et les nourritures,
+       choisi, par le joueur.
+    */
     public Node(Personnages p1,Personnages p2,Personnages p3,Personnages p4,Objet objet_possession,Nourriture nourriture_possession){
       this.p1 = p1;
       this.p2 = p2;
@@ -32,13 +62,10 @@ public class Node{
       this.nourriture_possession = nourriture_possession;
     }
 
-
-
-    //Quel attributs va t-on changer ?
-    // 1 -> description_debut_journee
-    // 2 -> description_expedition
-    // 3 -> description_scenario
-    public void set_description(int n,String description){
+    /**Mettre a jour les attributs de description
+      @param n  1:description_debut_journee    2:description_expedition   3:description_scenario
+    */
+    private void set_description(int n,String description){
       switch(n){
         case 1 :
             this.description_debut_journee = description;
@@ -51,18 +78,18 @@ public class Node{
             break;
       }
     }
-
-    public void set_nombre_journee(){
+    private void set_nombre_journee(){
       this.nombre_journee = this.nombre_journee + 1;
     }
 
-    //A completer
+
+    //A completer On va ici, choisir le scenario qu'il faut en fonction des personnages/objet/nourriture que l'on possede
     private static int choix_scenario(Personnages p1,Personnages p2,Personnages p3,Personnages p4,Objet o,Nourriture n){
       return 0;
     }
 
     //A completer avec les scenario qu'on veut
-    public void scenario(){
+    private void scenario(){
       int numero_scenario = choix_scenario(this.p1,this.p2,this.p3,this.p4,this.objet_possession,this.nourriture_possession);
 
       switch(numero_scenario){
@@ -74,19 +101,20 @@ public class Node{
     }
 
     //A completer : en fonction des persos, et des volonte des personnages, on peut ramener qql chose a l'exterieur
-    public void Expedition(){
+    private void expedition(){
     }
 
 
-    public void Nourir(){
+    private void nourir(){
       //Qui veut tu nourir ?
       //On affiche la nouriture disponible
       //On recupere qui le joeur veut nourir
       //On nourri en consequence
     }
 
-
-    //Est ce que on fait une fonction qui fait le deroulement de une journee ou pas ????
+    //On fait le deroulement d'une journee
+    public void deroulement_jour(){
+    }
 
 
 }
