245b693685b975dd8f2accb6199741283a084cb5
mathlesage <matheo.qtb@gmail.com>
2023-11-09 23:04:05 +0100

    Phase123h04

---
+++ projet/src/representation/Scenario_impose.java
@@ -0,0 +1,35 @@
+package Representation;
+import java.util.ArrayList;
+import univers.*;
+
+public class Scenario_impose {
+    private String histoire;
+    private int[] recompense1 = new int[4];
+    private int[] recompense2 = new int[4];
+    private int[] recompense3 = new int[4];
+    private int totalmental;
+    private int totalenergie;
+    private int totalsante;
+    private int totalforce;
+    private int totalintelligence;
+    private int totalresistance;
+    private int totalagilite;
+    public Scenario_impose(int[] recompense1, int[] recompense2, int[] recompense3, int[] recompense4, int totalmental, int totalenergie,int totalsante,int totalforce, int totalintelligence, int totalresistance, int totalagilite){
+        this.recompense1=recompense1;
+        this.recompense2=recompense2;
+        this.recompense3=recompense3;
+        this.totalmental=totalmental;
+        this.totalenergie=totalenergie;
+        this.totalsante=totalsante;
+        this.totalforce=totalforce;
+        this.totalintelligence=totalintelligence;
+        this.totalresistance=totalresistance;
+        this.totalagilite=totalagilite;
+    }
+    public int[] racontehistoire(Personnages personnages1, Personnages personnages2, Personnages personnages3, Personnages personnages4, Objet objet) {
+    
+        
+    return recompense1;
+
+}
+}
