24284064061a6552472690336a93de90f344e363
YacineAbdelouhab <118994777+YacineAbdelouhab@users.noreply.github.com>
2023-12-09 14:01:36 +0100

    Update Inventaire.java

--- projet/src/univers/Inventaire.java
+++ projet/src/univers/Inventaire.java
@@ -1,8 +1,17 @@
 package univers;
 
-
+/**
+Une classe implementant cette interface, aura pour role de simuler les objets/la nourriture presente ou pas dans la cave a un jour donne.
+@author QUATREBOEUFS MATHEO
+*/
 public interface Inventaire {
+
+    //Methode permettant d'avoir la quantite d'un objet/aliment precis
     int getQuantite(String nom);
+
+    //Methode permettant de mettre a jour la quantite d'un objet/aliment precis
     void setQuantite(String nom, int q);
+
+    //Methode permettant d'avoir la quantite de tout les objets/aliments
     void getQuantites();
-}  
+}
