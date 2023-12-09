d293b3f0a994336d3471bc6eb31854c77888f667
YacineAbdelouhab <118994777+YacineAbdelouhab@users.noreply.github.com>
2023-12-09 14:02:49 +0100

    Update Objet.java

--- projet/src/univers/Objet.java
+++ projet/src/univers/Objet.java
@@ -1,80 +1,95 @@
 package univers;
-
 import java.util.ArrayList;
 
-public class  Objet implements Inventaire {
-    protected ArrayList<Un_objet> inventaire = new ArrayList<Un_objet>();
-    public Objet() {Un_objet fusil = new Un_objet(NomObjet.FUSIL, 0);
+
+/**
+  Classe Objet, permet de connaitre le nombre des differents objets present dans la cave ou pas.
+  @author QUATREBOEUFS Matheo
+*/
+
+public class Objet implements Inventaire {
+
+    protected ArrayList<Un_element> inventaire = new ArrayList<Un_element>();
+
+    public Objet() {
+
+        Un_element fusil = new Un_element(Elements_du_jeu.FUSIL, 0);
         inventaire.add(fusil);
-        
-        Un_objet boiteOutils = new Un_objet(NomObjet.BOITE_OUTILS, 0);
+
+        Un_element boiteOutils = new Un_element(Elements_du_jeu.BOITE_OUTILS, 0);
         inventaire.add(boiteOutils);
-        
-        Un_objet katana = new Un_objet(NomObjet.KATANA, 0);
+
+        Un_element katana = new Un_element(Elements_du_jeu.KATANA, 0);
         inventaire.add(katana);
-        
-        Un_objet carteVille = new Un_objet(NomObjet.CARTE_VILLE, 0);
+
+        Un_element carteVille = new Un_element(Elements_du_jeu.CARTE_VILLE, 0);
         inventaire.add(carteVille);
-        
-        Un_objet echec = new Un_objet(NomObjet.ECHEC, 0);
+
+        Un_element echec = new Un_element(Elements_du_jeu.ECHEC, 0);
         inventaire.add(echec);
-        
-        Un_objet carteJoue = new Un_objet(NomObjet.CARTE_JOUE, 0);
+
+        Un_element carteJoue = new Un_element(Elements_du_jeu.CARTE_JOUE, 0);
         inventaire.add(carteJoue);
-        
-        Un_objet insecticide = new Un_objet(NomObjet.INSECTICIDE, 0);
+
+        Un_element insecticide = new Un_element(Elements_du_jeu.INSECTICIDE, 0);
         inventaire.add(insecticide);
-        
-        Un_objet hache = new Un_objet(NomObjet.HACHE, 0);
+
+        Un_element hache = new Un_element(Elements_du_jeu.HACHE, 0);
         inventaire.add(hache);
-        
-        Un_objet sac = new Un_objet(NomObjet.SAC, 0);
+
+        Un_element sac = new Un_element(Elements_du_jeu.SAC, 0);
         inventaire.add(sac);
-        
-        Un_objet trousse = new Un_objet(NomObjet.TROUSSE, 0);
+
+        Un_element trousse = new Un_element(Elements_du_jeu.TROUSSE, 0);
         inventaire.add(trousse);
-        
-        Un_objet radio = new Un_objet(NomObjet.RADIO, 0);
+
+        Un_element radio = new Un_element(Elements_du_jeu.RADIO, 0);
         inventaire.add(radio);
-        
-        Un_objet lampe = new Un_objet(NomObjet.LAMPE, 0);
+
+        Un_element lampe = new Un_element(Elements_du_jeu.LAMPE, 0);
         inventaire.add(lampe);
-        
-        Un_objet pile = new Un_objet(NomObjet.PILE, 0);
+
+        Un_element pile = new Un_element(Elements_du_jeu.PILE, 0);
         inventaire.add(pile);
     }
 
+    public int getQuantite(String nom) throws IllegalArgumentException{
+      for(Un_element obj : inventaire) {
+          if(nom.equals(obj.getNom())) {
+              return obj.getQuantite();
+          }
+      }
+      throw new IllegalArgumentException("Objet inexistant ! ");
+    }
 
-    public int getQuantite(String nom){
-        for(Un_objet obj : inventaire) {
-            if(nom.equals(obj.getnom())) {
-                return obj.getquantite();
+    public void setQuantite(String nom, int q) throws IllegalArgumentException,ArithmeticException{
+
+        boolean verif = false;
+        for(Un_element obj : inventaire) {
+            if(nom.equals(obj.getNom())) {
+                try{
+                  obj.setQuantite(q);
+                }
+                catch(ArithmeticException e){
+                  throw new ArithmeticException(e.getMessage());
+                }
+                verif = true;
             }
         }
-        return -1;
-}
-    public void setQuantite(String nom, int q) {
-        for(Un_objet obj : inventaire) {
-            if(nom.equals(obj.getnom())) {
-                obj.setQuantite(q);
-            }
+
+        if(!verif){
+          throw new IllegalArgumentException("Objet inexistant ! ");
         }
-
-
-}
+    }
 
     public void getQuantites() {
-        
-    for (Un_objet obj : inventaire) {
 
-        System.out.print(obj.getnom());
-        System.out.print(" ---> ");
-        System.out.print(obj.getquantite());
-        System.out.println(); // Saut de ligne entre chaque sous-liste
-}
-}
+      for (Un_element obj : inventaire) {
+          System.out.print(obj.getNom());
+          System.out.print(" ---> ");
+          System.out.print(obj.getQuantite());
+          System.out.println(); // Saut de ligne entre chaque sous-liste
+      }
+    }
 
-
-
-    
 }
