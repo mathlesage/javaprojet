fa5b42ef4f99c7bae8b651022eb5e7fc1b14b53b
YacineAbdelouhab <118994777+YacineAbdelouhab@users.noreply.github.com>
2023-12-22 22:37:42 +0100

    Update TerminalNode.java

--- projet/src/representation/TerminalNode.java
+++ projet/src/representation/TerminalNode.java
@@ -4,8 +4,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+
+
+/** Classe TerminalNode
+ * @author QUATREBOEUFS Matheo
+*/
 public class TerminalNode extends Node {
 
+	/**Constructeur
+	 * @param histoire histoire du terminal node
+	 * @param id id du terminal node
+	 * */
     TerminalNode(String histoire, int id) {
         super(histoire, id);
     }
